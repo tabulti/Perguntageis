8b37dd3c89e189172501247341cd086fecceb328
GermanoO <bgermano89@gmail.com>
2015-10-30 11:28:05 -0300

    teste

--- Cap3/app/src/main/java/com/joaopaulo/cap3/fragments/BaseFragment.java
+++
@@ -1,8 +0,0 @@
-package com.joaopaulo.cap3.fragments;
-
-/**
- * Created by joao.paulo.d.ribeiro on 22/10/2015.
- */
-public class BaseFragment extends livroandroid.lib.fragment.BaseFragment {
-    //TODO
-}
