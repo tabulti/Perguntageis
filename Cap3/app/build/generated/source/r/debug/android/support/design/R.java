a09aefa7381ac8af16920db8fe07c1face8cf864
Gustavo Henrique P S de Barros <gustavohbarros.1992@gmail.com>
2015-11-04 22:57:32 -0200

    status+firebase

--- Cap3/app/build/generated/source/r/debug/android/support/design/R.java
+++ Cap3/app/build/generated/source/r/debug/android/support/design/R.java
@@ -564,9 +564,9 @@ public final class R {
 		public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f020042;
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f020043;
 		public static final int abc_textfield_search_material = 0x7f020044;
-		public static final int design_fab_background = 0x7f02005e;
-		public static final int design_snackbar_background = 0x7f02005f;
-		public static final int notification_template_icon_bg = 0x7f02008a;
+		public static final int design_fab_background = 0x7f02005f;
+		public static final int design_snackbar_background = 0x7f020060;
+		public static final int notification_template_icon_bg = 0x7f02008b;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f0d00a6;
--- Cap3/app/src/main/java/com/joaopaulo/cap3/activity/PopupStatus.java
+++ Cap3/app/src/main/java/com/joaopaulo/cap3/activity/PopupStatus.java
@@ -3,17 +3,36 @@ package com.joaopaulo.cap3.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import com.firebase.client.ValueEventListener;
 import com.joaopaulo.cap3.R;
+
+import java.util.HashMap;
 
 /**
  * Created by Gustavo on 03/11/2015.
  */
+
+
+
 public class PopupStatus extends Activity {
+
+
+    private ImageView aux;
+
+    final private Firebase fbstatus = new Firebase("https://resplendent-heat-382.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
 
         setContentView(R.layout.popup_status);
         DisplayMetrics dm = new DisplayMetrics();
@@ -22,7 +41,134 @@ public class PopupStatus extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int) (width*.8),(int) (height*.4));
+        getWindow().setLayout((int) (width*.8),(int) (height*.47));
+
+        apagarCorrects();
+
+        fbstatus.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                HashMap<String,String> jogo= (HashMap)dataSnapshot.child("jogos").child("jogoexample").getValue();
+                int leana = Integer.parseInt(jogo.get("leanA"));
+
+                if(leana == 0){
+
+                }else if(leana == 1){
+                    aux = (ImageView) findViewById(R.id.leana1);
+                    aux.setVisibility(View.VISIBLE);
+                }else if (leana == 2){
+                    aux = (ImageView) findViewById(R.id.leana1);
+                    aux.setVisibility(View.VISIBLE);
+
+                    aux = (ImageView) findViewById(R.id.leana2);
+                    aux.setVisibility(View.VISIBLE);
+                }else{
+                    aux = (ImageView) findViewById(R.id.leana1);
+                    aux.setVisibility(View.VISIBLE);
+
+                    aux = (ImageView) findViewById(R.id.leana2);
+                    aux.setVisibility(View.VISIBLE);
+
+                    aux = (ImageView) findViewById(R.id.leana3);
+                    aux.setVisibility(View.VISIBLE);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
 
     }
+
+
+
+
+
+
+
+
+    public void apagarCorrects(){
+
+        aux = (ImageView) findViewById(R.id.agilea1);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.agilea2);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.agilea3);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.agileb1);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.agileb2);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.agileb3);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.scruma1);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.scruma2);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.scruma3);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.scrumb1);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.scrumb2);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.scrumb3);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.xpa1);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.xpa2);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.xpa3);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.xpb1);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.xpb2);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.xpb3);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.leana1);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.leana2);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.leana3);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.leanb1);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.leanb2);
+        aux.setVisibility(View.INVISIBLE);
+
+        aux = (ImageView) findViewById(R.id.leanb3);
+        aux.setVisibility(View.INVISIBLE);
+
+
+
+    }
+
+
 }
