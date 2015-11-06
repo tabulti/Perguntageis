6403adeb079714b4e8f45d63e50a20338ba233fb
Gustavo Henrique P S de Barros <gustavohbarros.1992@gmail.com>
2015-11-06 02:43:52 -0200

    buscar pergunta no Firebase, exibir pergunta e voltar pra tela de Roleta

--- Cap3/app/src/main/java/com/joaopaulo/cap3/activity/PerguntaActivity.java
+++ Cap3/app/src/main/java/com/joaopaulo/cap3/activity/PerguntaActivity.java
@@ -9,10 +9,15 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 import com.joaopaulo.cap3.R;
 
 import java.util.HashMap;
@@ -21,6 +26,8 @@ import java.util.HashMap;
 public class PerguntaActivity extends AppCompatActivity {
     private int jumpTime;
 
+    private Button b1,b2,b3,b4;
+    private TextView pergunta;
 
     private ProgressBar timer;
     final private Firebase ref = new Firebase("https://resplendent-heat-382.firebaseio.com/");
@@ -33,6 +40,31 @@ public class PerguntaActivity extends AppCompatActivity {
         final Handler mHandler = new Handler();
         final int progressTime = 100;
 
+        b1 = (Button) findViewById(R.id.btn1);
+        b2 = (Button) findViewById(R.id.btn2);
+        b3 = (Button) findViewById(R.id.btn3);
+        b4 = (Button) findViewById(R.id.btn4);
+        pergunta = (TextView) findViewById(R.id.pergunta);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                HashMap<String, String> p = (HashMap) snapshot.child("perguntas").child("scrum02").getValue();
+                pergunta.setText(p.get("pergunta"));
+                b1.setText(p.get("alternativaCorreta"));
+                b2.setText(p.get("alternativaA"));
+                b3.setText(p.get("alternativaB"));
+                b4.setText(p.get("alternativaC"));
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+
+
         final Thread t = new Thread(){
 
             @Override
@@ -42,7 +74,7 @@ public class PerguntaActivity extends AppCompatActivity {
                 jumpTime = 0;
                 while(jumpTime < progressTime){
                     try {
-                        sleep(50);
+                        sleep(150);
                         jumpTime += 1;
                         timer.setProgress(jumpTime);
                     } catch (InterruptedException e) {
@@ -94,10 +126,16 @@ public class PerguntaActivity extends AppCompatActivity {
     }
 
     @Override
-    public void onPause(){
+    protected void onPause(){
         super.onPause();
         jumpTime = 0;
 
+    }
+
+    public void responder(){
+
+
+        onBackPressed();
     }
 
 
